30669e0deb43678f053f2a4fb8e2c3a224fc512d
nmaju terence <nmajutee@gmail.com>
2025-07-08 08:41:15 -0400

    Simple Interest Calculator

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -4,7 +4,7 @@ rate = 0.05 # Rate of interest (5%)
 time = 3  # Time in years
 
 # Calculate simple interest
-interest = (principal * rate * time)
+interest = principal * rate * time
 
 # Display the result
 print(f"The simple interest is {interest}")
