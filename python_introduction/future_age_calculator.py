27bf91bbb96c7b50b4f5410cd26dc89212d71eca
Chikwado stanley Elochukwu <chikwadostanley0@gmail.com>
2025-07-13 18:07:45 -0700

    Update future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -2,6 +2,3 @@ current_age = int(input("How old are you? "))
 future_age = current_age + 27
 
 print(f"In 2050, you will be {future_age} years old.")
-monthly_income = float(input("Enter your monthly income: "))
-monthly_expenses = float(input("Enter your total monthly expenses: "))
-...
