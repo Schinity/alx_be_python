4f16e3b740f25ef4af14ebd55b530b6bfb62f127
Chikwado stanley Elochukwu <chikwadostanley0@gmail.com>
2025-07-13 17:25:24 -0700

    Update future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -3,7 +3,7 @@ monthly_expenses = float(input("Enter your total monthly expenses: "))
 monthly_savings = monthly_income - monthly_expenses
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 input("How old are you? ")
-current_age = int(input("How old are you? "))
+current_age = float(input("How old are you? "))
 future_age = current_age + 27
 
 print(f"In 2050, you will be {future_age} years old.")
